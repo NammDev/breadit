f58c6d47f3822c0092b7da6e3270a22da9cc4cff
Nam <namdeveloper.ca@gmail.com>
2023-09-25 14:16:52 +0700

    clone start repo

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['uploadthing.com', 'lh3.googleusercontent.com'],
+  },
+  experimental: {
+    serverActions: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 
 module.exports = nextConfig
